ee7380d38a8e8353c440472c2d95e3b0fd0dda91
Roman Tsiupa <draconyster@gmail.com>
2014-02-09 11:27:22 +0200

    steps

--- classes/PHPixie/ORM/Planners/Steps/Step/Query.php
+++ classes/PHPixie/ORM/Planners/Steps/Step/Query.php
@@ -6,6 +6,10 @@ class Query extends \PHPixie\ORM\Query\Plan\Step {
 	
 	protected $query;
 	
+	public function __construct($query) {
+		$this->query = $query;
+	}
+	
 	public function execute() {
 		$this->query->execute();
 	}
