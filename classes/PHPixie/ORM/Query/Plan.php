c259d0aa93bc26cf55d4eb78e3e34dd002029882
dracony <draconyster@gmail.com>
2014-02-19 18:54:54 +0200

    preloaders

--- classes/PHPixie/ORM/Query/Plan.php
+++ classes/PHPixie/ORM/Query/Plan.php
@@ -5,26 +5,13 @@ namespace \PHPixie\ORM\Query;
 class Plan {
 	
 	protected $steps = array();
-	protected $result;
 	
 	public function push($step) {
 		$this->steps[] = $step;
 	}
 	
-	public function pop() {
-		return array_pop($this->steps);
-	}
-	
-	public function unshift($step) {
-		array_unshift($this->steps, $step);
-	}
-	
-	public function shift() {
-		return array_shift($this->steps);
-	}
-	
-	public function prepend_plan($plan) {
-		$this->steps = array_merge($plan->steps(), $this->steps);
+	public function append_plan($plan) {
+		$this->steps = array_merge($this->steps, $plan->steps());
 	}
 	
 	public function steps() {
@@ -35,12 +22,6 @@ class Plan {
 		foreach($this->steps as $step) {
 			$step->execute();
 		}
-		
-		if ($step instanceof \PHPixie\ORM\Query\Plan\Step\ResultQuery)
-			$this->result = $step->result();
 	}
 	
-	public function result() {
-		return $this->result();
-	}
 }
